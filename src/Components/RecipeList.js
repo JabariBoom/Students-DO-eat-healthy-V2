c98459d845ae0fde5a773637be150d0554dec69b
jabaringure@gmail.com <jabaringure@gmail.com>
2024-11-14 10:34:28 +0300

    Recipe App Progress 9

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -3,17 +3,20 @@ import RecipeItem from './RecipeItem';
 
 const RecipeList = ({ recipes, onSelectRecipe, onDeleteRecipe }) => {
   return (
-    <div className="recipe-list">
-      {recipes.map(recipe => (
-        <RecipeItem
-          key={recipe.title}
-          recipe={recipe}
-          onSelect={onSelectRecipe}
-          onDelete={onDeleteRecipe}
-        />
-      ))}
+    <div className="button-container">
+      <div className="recipe-list">
+        {recipes.map(recipe => (
+          <RecipeItem
+            key={recipe.title}
+            recipe={recipe}
+            onSelect={onSelectRecipe}
+            onDelete={onDeleteRecipe}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default RecipeList;
+
